741c1e45864734f1f382d81597c506329e349fc0
jbock <jbock-java@gmx.de>
2024-07-09 03:12:05 -0400

    tabs to spaces

--- src/main/client/src/App.jsx
+++ src/main/client/src/App.jsx
@@ -1,23 +1,26 @@
-import { useEffect, useState } from "react";
+import {
+  useEffect,
+  useState,
+} from "react"
 
 export const App = () => {
-	let [ data, setData ] = useState("");
-	useEffect(() => {
-		let timer = setInterval(async () => {
-			let d = await fetchAsync("/app/data");
-			setData(d);
-		}, 2000);
-		return () => clearInterval(timer);
-	});
-	return (
-		<div className="App">
-			<h1 className="text-3xl font-bold underline">{data}</h1>
-		</div>
-	);
+  let [ data, setData ] = useState("")
+  useEffect(() => {
+    let timer = setInterval(async () => {
+      let d = await fetchAsync("/app/data")
+      setData(d)
+    }, 2000)
+    return () => clearInterval(timer)
+  })
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">{data}</h1>
+    </div>
+  )
 }
 
 async function fetchAsync (url) {
-	let response = await fetch(url);
-	let data = await response.json();
-	return data;
+  let response = await fetch(url)
+  let data = await response.json()
+  return data;
 }
--- src/main/client/vite.config.js
+++ src/main/client/vite.config.js
@@ -3,16 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	build: {
-		outDir: '../resources/public/',
-		emptyOutDir: true,
-	},
-	base: '/app',
-	server: {
-		port: 3006,
-		proxy: {
-			'/app/data': 'http://localhost:8080',
-		},
-	},
+  plugins: [react()],
+  build: {
+    outDir: '../resources/public/',
+    emptyOutDir: true,
+  },
+  base: '/app',
+  server: {
+    port: 3006,
+    proxy: {
+      '/app/data': 'http://localhost:8080',
+    },
+  },
 })
