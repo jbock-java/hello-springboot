14c9813b0957dbb61faf9ba54e60a11f2338feea
jbock <jbock-java@gmx.de>
2024-08-16 10:01:24 +0200

    wip chat users

--- src/main/client/src/feature/lobby/LobbyPanel.jsx
+++ src/main/client/src/feature/lobby/LobbyPanel.jsx
@@ -12,13 +12,16 @@ import {
   useAuthStore,
 } from "../../store.js"
 import {
+  Chat,
+} from "../../component/Chat.jsx"
+import {
   SideBar,
 } from "../../component/SideBar.jsx"
 
 export const LobbyPanel = () => {
   return (
     <SideBar page="lobby">
-      <div className="pt-2">
+      <div className="pr-3 pt-4 pl-2 h-full flex flex-col gap-y-1">
         <Panel />
       </div>
     </SideBar>
@@ -48,18 +51,14 @@ function Panel() {
       sub1.unsubscribe()
     }
   }, [setUsers, stompClient])
-  return (
-    <div className="mt-2">
-      <div className="pl-2 pb-2 border-b border-b-slate-700">
-        {auth.name}
-      </div>
-      <div className="pl-2 mt-2">
-        {users.map(user => (
-          <div key={user}>
-            {user}
-          </div>
-        ))}
-      </div>
+  return <>
+    <div className="flex-none h-12 border border-gray-500 bg-gray-900 rounded-lg p-1 overflow-y-scroll">
+      {users.map(user => (
+        <div key={user}>
+          {user}
+        </div>
+      ))}
     </div>
-  )
+    <Chat chatId="Lobby"/>
+  </>
 }
