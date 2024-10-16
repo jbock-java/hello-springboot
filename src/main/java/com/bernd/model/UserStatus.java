fbf6b6f5b98c976472d87ac45afda913be5b523a
jbock <jbock-java@gmx.de>
2024-10-16 15:00:09 +0200

    refactoring, add OpenGame to UserStatus

--- src/main/java/com/bernd/model/UserStatus.java
+++ src/main/java/com/bernd/model/UserStatus.java
@@ -2,19 +2,62 @@ package com.bernd.model;
 
 public record UserStatus(
         String room,
-        long lastSeen) {
+        long lastSeen,
+        OpenGame openGame) {
 
+    // max interval between heartbeats
     private static final long USER_TIMEOUT = 45 * 1000;
 
     public static UserStatus create(String room) {
-        return new UserStatus(room, System.currentTimeMillis());
+        return new UserStatus(
+                room,
+                System.currentTimeMillis(),
+                null);
     }
 
     public UserStatus touch() {
-        return new UserStatus(room, System.currentTimeMillis());
+        return toBuilder().lastSeen(System.currentTimeMillis()).build();
     }
 
     public boolean isActive(long current) {
         return current < lastSeen + USER_TIMEOUT;
+    }
+
+    public Builder toBuilder() {
+        return new Builder(room, lastSeen, openGame);
+    }
+
+    public static final class Builder {
+        private String room;
+        private long lastSeen;
+        private OpenGame openGame;
+
+        private Builder(
+                String room,
+                long lastSeen,
+                OpenGame openGame) {
+            this.room = room;
+            this.lastSeen = lastSeen;
+            this.openGame = openGame;
+        }
+
+        public Builder room(String room) {
+            this.room = room;
+            return this;
+        }
+
+        public Builder lastSeen(long lastSeen) {
+            this.lastSeen = lastSeen;
+            return this;
+        }
+
+        public Builder openGame(OpenGame openGame) {
+            this.openGame = openGame;
+            return this;
+        }
+
+        public UserStatus build() {
+            return new UserStatus(room, lastSeen, openGame);
+        }
     }
 }
